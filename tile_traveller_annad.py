da1a7a16109b029491a5e1bcc3c445e10148f7e2
Sakarías Nói Fjalar Ragnarsson <sakarias@Sakariass-MacBook-Pro.local>
2019-10-08 15:02:43 +0000

    final push

--- tile_traveller_annad.py
+++ tile_traveller_annad.py
@@ -3,9 +3,8 @@ NORTH = 'n'
 EAST = 'e'
 SOUTH = 's'
 WEST = 'w'
-
+ 
 def move(direction, col, row):
-    ''' Returns updated col, row given the direction '''
     if direction == NORTH:
         row += 1
     elif direction == SOUTH:
@@ -15,11 +14,10 @@ def move(direction, col, row):
     elif direction == WEST:
         col -= 1
     return(col, row)    
-
+ 
 def is_victory(col, row):
-    ''' Return true is player is in the victory cell '''
     return col == 3 and row == 1 # (3,1)
-
+ 
 def print_directions(directions_str):
     print("You can travel: ", end='')
     first = True
@@ -36,9 +34,8 @@ def print_directions(directions_str):
             print("(W)est", end='')
         first = False
     print(".")
-        
+         
 def find_directions(col, row):
-    ''' Returns valid directions as a string given the supplied location '''
     if col == 1 and row == 1:   # (1,1)
         valid_directions = NORTH
     elif col == 1 and row == 2: # (1,2)
@@ -56,52 +53,41 @@ def find_directions(col, row):
     elif col == 3 and row == 3: # (3,3)
         valid_directions = SOUTH+WEST
     return valid_directions
-
-
-
-def pull_lever(col, row, coins):
-
-
-    if (col, row) in [(1,2),(2,2),(2,3),(3,2)]:
-        answer = input("Pull a lever (y/n): ").lower()
-        if answer == "y":
-            coins += 1
-            print("You received 1 coin, your total is now {}.".format(coins))
-    
-    return coins
-
-
-def play_one_move(col, row, valid_directions, coins):
-    ''' Plays one move of the game
-        Return if victory has been obtained and updated col,row '''
-    victory = False
-    direction = input("Direction: ")
-    direction = direction.lower()
-    
-    if not direction in valid_directions:
-        print("Not a valid direction!")
-    else:
-        col, row = move(direction, col, row)
-        victory = is_victory(col, row)
-        coins = pull_lever(col, row, coins)
-    return victory, col, row, coins
-
+ 
+def coins(lever, total_coins):
+    if (col == 1 and row == 2) or (col == 2 and row ==2) or (col == 2 and row == 3) or (col == 3 and row == 2):
+        lever = input("Pull a lever (y/n): ")
+        lever = lever.lower()
+        if lever == "y":
+            total_coins += 1
+            print("You received 1 coin, your total is now {}.".format(total_coins))
+    return total_coins
+ 
 # The main program starts here
 victory = False
 row = 1
 col = 1
-coins = 0
-
+lever = ""
+total_coins = 0
+ 
 valid_directions = NORTH
 print_directions(valid_directions)
-
+ 
 while not victory:
-    victory, col, row, coins = play_one_move(col, row, valid_directions, coins)
-    if victory:
-        print("Victory! Total coins {}.".format(coins))
+    direction = input("Direction: ")
+    direction = direction.lower()
+     
+    if not direction in valid_directions:
+        print("Not a valid direction!")
     else:
-        valid_directions = find_directions(col, row)
-        print_directions(valid_directions)
+        col, row = move(direction, col, row)
+        total_coins = coins(lever, total_coins)
+ 
+        victory = is_victory(col, row)
+        if victory:
+            print("Victory! Total coins {}.".format(total_coins))
+        else:
+            valid_directions = find_directions(col, row)
+            print_directions(valid_directions)
 
-
-        #kfkfkfkfk
+            
